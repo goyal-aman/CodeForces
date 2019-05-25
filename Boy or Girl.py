463c5469156f796e0a79b330e72ec7ded2ea7748
Aman2 <aman-goyal>
2019-05-25 17:16:56 +0530

    Stones on the Table-better version

--- Boy or Girl.py
+++ Boy or Girl.py
@@ -1,7 +1,1 @@
-bog = input().lower()
-temp = ''
-for i in bog:
-    if not i in temp:
-        temp += i
-
-print(['CHAT WITH HER!','IGNORE HIM!'][len(temp)%2!=0])
+print(  ('CHAT WITH HER!','IGNORE HIM!')[ len( set( input() )  )%2  ]    )
