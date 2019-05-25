06c1841a88b9b5aa20bc0dcf9947f3f108dc5084
Aman2 <aman-goyal>
2019-05-25 16:42:40 +0530

    Stones on the Table-better version

--- Stones on the Table.py
+++ Stones on the Table.py
@@ -1,8 +1,3 @@
-num = int(input())
+num = input()
 stone = input()
-remove,i = 0,0
-while i < num-1:
-    if stone[i] ==  stone[i+1]:
-        remove+=1
-    i+=1
-print(remove)
+print( sum(a==b for a,b in zip(stone[1:],stone)) )
