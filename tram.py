6bbb0034edd21b6600a4badbe2cdfec2b43df329
Aman2 <aman-goyal>
2019-05-26 14:18:15 +0530

    tram better looking solution ( consumes more memory)

--- tram.py
+++ tram.py
@@ -1,9 +1,5 @@
-a = int(input())
-capacity, temp = 0, 0
-temp2 = 0
-for _ in range(a):
-    s = list(map(int, input().split()))
-    temp += s[1] - s[0]
-    if temp > capacity:
-        capacity = temp
+capacity = temp = 0
+for _ in [0]*int(input()):
+    temp-=eval(input().replace(' ','-'))
+    capacity = max(temp, capacity)
 print(capacity)
