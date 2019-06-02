0847faf7b0e90e1c0baec1e6c28c6076da43387c
Aman2 <aman-goyal>
2019-06-03 01:58:15 +0530

    chat room alternate solution

--- chat room.py
+++ chat room.py
@@ -1,9 +1,14 @@
-s = input()
-i = -1
-d = ['h','e','l','l','o']
-for ch in d:
-    i = s.find(ch, i+1)
-    if i == -1:
-        print("NO")
-        exit()
-print("YES")
+# s = input()
+# i = -1
+# d = ['h','e','l','l','o']
+# for ch in d:
+#     i = s.find(ch, i+1)
+#     if i == -1:
+#         print("NO")
+#         exit()
+# print("YES")
+
+# alternate solution
+
+s = iter(input())
+print('NYOE S'[all(c in s for c in 'hello')::2])
