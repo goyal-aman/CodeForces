b67c7b0eff4bdc20e206a1de03b0b6e1bd9cc6bf
Aman2 <aman-goyal>
2019-06-04 16:36:03 +0530

    single line solution

--- Nearly Lucky Number.py
+++ Nearly Lucky Number.py
@@ -1,6 +1,10 @@
-s = input()
-out = 0
-for ch in s:
-    if ch == '4' or ch == '7':
-        out +=1
-print(['NO','YES'][any(out == i for i in [4,7])])
+print('NYOE S'[sum(i in '47' for i in input()) in [4,7]::2])
+
+
+# s = input()
+# out = 0
+# for ch in s:
+#     if ch == '4' or ch == '7':
+#         out +=1
+# print(['NO','YES'][any(out == i for i in [4,7])])
+
