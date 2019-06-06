d7b91a46a8dd359d12dd1c45c9fd39d741e7849e
Aman2 <aman-goyal>
2019-06-07 00:34:51 +0530

    dubstep completed

--- Soldier and Bananas.py
+++ Soldier and Bananas.py
@@ -1,8 +1,4 @@
 #k the cost of the first banana
 # n initial number of dollars the soldier
 #w number of bananas he wants
-k, n, w = map(int, input().split())
-count = 0
-for i in range(1,w+1):
-    count += i*k
-print([0,count - n][count > n])
+
