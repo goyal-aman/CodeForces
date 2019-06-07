9ac697eb010b9a82f72c1695cf7724c871a6c5d7
Aman2 <aman-goyal>
2019-06-07 22:05:09 +0530

    Ultr-fast mathematician better solution

--- Ultra-Fast Mathematician.py
+++ Ultra-Fast Mathematician.py
@@ -1,7 +1,12 @@
 s = input()
 p = input()
-for i in range(len(s)):
-    if s[i] != p[i]:
-        print('1',end='')
-    else:
-        print('0',end='')
+print(''.join('01'[i!=j]for i,j in zip(s,p)))
+
+
+# s = input()
+# p = input()
+# for i in range(len(s)):
+#     if s[i] != p[i]:
+#         print('1',end='')
+#     else:
+#         print('0',end='')
