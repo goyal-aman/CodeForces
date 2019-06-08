101e16120d1f6509cc3e3816d68798198df78980
Aman2 <aman-goyal>
2019-06-09 02:30:52 +0530

    Anton and Letters Better SOlution | One liner Completed

--- Anton and Letters.py
+++ Anton and Letters.py
@@ -1,6 +1,8 @@
-a = set(input())
-str = ''
-for ch in a:
-    if ord(ch) >= ord('a') and ord(ch) <= ord('z'):
-        str += ch
-print(len(str))
+print(len(set(input()+ ', '))-4)
+
+# a = set(input())
+# str = ''
+# for ch in a:
+#     if ord(ch) >= ord('a') and ord(ch) <= ord('z'):
+#         str += ch
+# print(len(str))
