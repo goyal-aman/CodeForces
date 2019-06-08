4f27d9067d87139178a455472fd635f7b21a52b2
Aman2 <aman-goyal>
2019-06-09 01:10:02 +0530

    Game with Sticks One Liner Completed

--- Game With Sticks.py
+++ Game With Sticks.py
@@ -1,9 +1,12 @@
-# n -
-# m |
 n,m = map(int, input().split())
-turn = 0
-while n and m:
-    turn +=1 #odd turn : akshat
-    n-=1
-    m-=1
-print(['Akshat','Malvika'][turn%2==0])
+print(['Akshat','Malvika'][min(n,m)%2==0])
+
+# # n -
+# # m |
+# n,m = map(int, input().split())
+# turn = 0
+# while n and m:
+#     turn +=1 #odd turn : akshat
+#     n-=1
+#     m-=1
+# print(['Akshat','Malvika'][turn%2==0])
