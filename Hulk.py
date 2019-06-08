7dade272d5209fb0dc0f050dadef90ae7aed8f44
Aman2 <aman-goyal>
2019-06-09 02:07:33 +0530

    Hulk alternative solution Completed
    *First solution is better in performance in terms of time.

--- Hulk.py
+++ Hulk.py
@@ -1,6 +1,10 @@
-l = [' I love', ' I hate']
 n = int(input())
-str = ''
-for i in range(1,n+1):
-    str += l[i%2] + ' that'
-print(str[:len(str)-5] + ' it')
+l = ['I love', 'I hate']
+a = l*n
+print(' that '.join(a[:n])+ ' it')
+# l = [' I love', ' I hate']
+# n = int(input())
+# str = ''
+# for i in range(1,n+1):
+#     str += l[i%2] + ' that'
+# print(str[:len(str)-5] + ' it')
