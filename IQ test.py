5897d4a2a1accbd46776b65e8788db336b957e25
Aman2 <aman-goyal>
2019-06-09 01:43:16 +0530

    IO test better solution completed

--- IQ test.py
+++ IQ test.py
@@ -1,15 +1,19 @@
-n = int(input())
-lst = list(map(int, input().split()))
-odd, even = 0, 0
+input()
+s = list(map(lambda x: int(x) % 2, input().split()))
+print(s.index(sum(s) == 1)+1)
 
-for i in range(3):
-    if lst[i]%2:
-        odd+=1
-if odd>1:
-    odd = 0
-else:
-    odd = 1
-for ch in lst:
-    if ch%2==odd:
-        print(lst.index(ch)+1)
-        break
+# n = int(input())
+# lst = list(map(int, input().split()))
+# odd, even = 0, 0
+#
+# for i in range(3):
+#     if lst[i]%2:
+#         odd+=1
+# if odd>1:
+#     odd = 0
+# else:
+#     odd = 1
+# for ch in lst:
+#     if ch%2==odd:
+#         print(lst.index(ch)+1)
+#         break
