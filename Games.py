62b0e047f75ba71a28a9fce9c229c5d517bcbc89
Aman2 <aman-goyal>
2019-06-09 22:19:52 +0530

    Games Alternate Solution Completed

--- Games.py
+++ Games.py
@@ -1,12 +1,16 @@
 n = int(input())
-s=[]
-count = 0
-for _ in range(n):
-    s.append(list(map(int, input().split())))
-for i in range(n):
-    t = s[i][0]
-    for j in range(n):
-        p = s[j][1]
-        if t == p:
-            count +=1
-print(count)
+a,b = zip(*[input().split() for _ in ' '*n])
+print(sum(b.count(x) for x in a))
+
+# n = int(input())
+# s=[]
+# count = 0
+# for _ in range(n):
+#     s.append(list(map(int, input().split())))
+# for i in range(n):
+#     t = s[i][0]
+#     for j in range(n):
+#         p = s[j][1]
+#         if t == p:
+#             count +=1
+# print(count)
