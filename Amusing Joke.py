ec09b1c061a475a7c165f12c27940d25660bd953
Aman2 <aman-goyal>
2019-06-10 00:36:01 +0530

    Amusing Joke One Liner | Better Solution Completed

--- Amusing Joke.py
+++ Amusing Joke.py
@@ -1,4 +1,6 @@
-str1 = input()
-str2 =  sorted(input() + str1)
-str3 = sorted(input())
-print(['NO','YES'][str2==str3])
+print('YNEOS'[sorted(input()+input()) != sorted(input()) ::2])
+
+# str1 = input()
+# str2 =  sorted(input() + str1)
+# str3 = sorted(input())
+# print(['NO','YES'][str2==str3])
