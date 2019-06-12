666903bbf51660b3c67375a0ba8b6e0527020026
Aman2 <aman-goyal>
2019-06-13 00:11:30 +0530

    Dragons Alternate Solution Completed

--- Dragons.py
+++ Dragons.py
@@ -1,15 +1,22 @@
-player_strength, lvl = map(int, input().split())
-data_input = []
-for _ in ' '*lvl:
-    data_input.append(list(map(int, input().split())))
+I = lambda:map(int, input().split())
+strength, lvl = I()
+for drgn_strn, bonus in sorted(list(I()) for _ in ' '*lvl):
+    strength = (strength + bonus) * (strength > drgn_strn)
+print('YNEOS'[strength==0::2])
 
-data_input.sort(key= lambda x: (x[0]))
 
-for i in data_input:
-    if player_strength > i[0]:
-        player_strength+=i[1]
-    else:
-        print('NO')
-        exit()
-else:
-    print("YES")
+# player_strength, lvl = map(int, input().split())
+# data_input = []
+# for _ in ' '*lvl:
+#     data_input.append(list(map(int, input().split())))
+#
+# data_input.sort(key= lambda x: (x[0]))
+#
+# for i in data_input:
+#     if player_strength > i[0]:
+#         player_strength+=i[1]
+#     else:
+#         print('NO')
+#         exit()
+# else:
+#     print("YES")
