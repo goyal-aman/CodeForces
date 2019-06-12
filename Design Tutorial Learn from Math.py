067eaa1c0f822d41900898409b0518a3885a6b43
Aman2 <aman-goyal>
2019-06-12 21:24:33 +0530

    Design Tutorial Learn from Math Completed

--- Design Tutorial Learn from Math.py
+++ Design Tutorial Learn from Math.py
@@ -1,3 +1,17 @@
-# solution subtion on codeforces
-# 33613462
-# 37342142
+
+def CheckPrime(n):
+    i, count = 1,0
+    while i<= n:
+        count += 1 if n%i==0 else 0
+        i+=1
+    return count<3
+
+n = int(input())
+while True:
+    temp = 4
+    while temp < n:
+        # print(CheckPrime(temp), CheckPrime(n-temp))
+        if  (not CheckPrime(temp) and not CheckPrime(n-temp)):
+            print(temp, n-temp)
+            exit()
+        temp +=1
