7126a819b802eb0311c5bb71d33c343678b8ee8a
Aman2 <aman-goyal>
2019-06-15 05:02:10 +0530

    New Year transportaion Best Solution Completed

--- New Year Transportation.py
+++ New Year Transportation.py
@@ -1,12 +1,36 @@
-n, t = map(int, input().split())
-s = list(map(int , input().split()))
-tile = 1
-for i in s:
-    while tile<=t:
-        tile += s[tile-1]
-        if tile == t:
-            print('YES')
-            exit()
-    else:
-        print('NO')
-        exit()
+n,t = map(int , input().split())
+s = list(map(int, input().split()))
+i = 1
+while i < t:
+    i+=s[i-1]
+print('YNEOS'[i>t ::2])
+
+
+# n, t = map(int, input().split())
+# s = list(map(int , input().split()))
+# tile = 1
+# try:
+#     for i in s:
+#         tile += s[tile - 1]
+#         if tile == t:
+#             print('YES')
+#             exit()
+#     else:
+#         print('NO')
+# except IndexError:
+#     print('NO')
+#
+
+
+# n, t = map(int, input().split())
+# s = list(map(int , input().split()))
+# tile = 1
+# for i in s:
+#     while tile<=t:
+#         tile += s[tile-1]
+#         if tile == t:
+#             print('YES')
+#             exit()
+#     else:
+#         print('NO')
+#         exit()
