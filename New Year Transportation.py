2e2380d05aabc1082dc631086f6308d5b5cde6a4
Aman2 <aman-goyal>
2019-06-15 04:51:11 +0530

    New Year transportaion Alternate Solution Completed

--- New Year Transportation.py
+++ New Year Transportation.py
@@ -1,13 +1,12 @@
 n, t = map(int, input().split())
 s = list(map(int , input().split()))
 tile = 1
-try:
-    for i in s:
-        tile += s[tile - 1]
+for i in s:
+    while tile<=t:
+        tile += s[tile-1]
         if tile == t:
             print('YES')
             exit()
     else:
         print('NO')
-except IndexError:
-    print('NO')
+        exit()
