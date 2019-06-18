bb5ec87d6ae3723209a6c0aa8f31c4d517b53790
Aman2 <aman-goyal>
2019-06-18 21:08:27 +0530

    I_love_%username% Alternate SOlution Completed

--- I_love_%username%.py
+++ I_love_%username%.py
@@ -1,8 +1,18 @@
 n = int(input())
 s = list(map(int, input().split()))
-amazing = 0
-for i in range(n):
-    if i > 0 and not s[i] in s[:i]:
-        if s[i] < min(s[:i]) or s[i] > max(s[:i]):
-            amazing +=1
-print(amazing)
+a = [s[0]]
+
+for i in s:
+    n -= min(a)<= i <= max(a)
+    a += [i]
+print(n)
+
+
+# n = int(input())
+# s = list(map(int, input().split()))
+# amazing = 0
+# for i in range(n):
+#     if i > 0 and not s[i] in s[:i]:
+#         if s[i] < min(s[:i]) or s[i] > max(s[:i]):
+#             amazing +=1
+# print(amazing)
