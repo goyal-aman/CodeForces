e4a92fff5f7d9b60a32e7e94b994ca7804fc3150
Aman2 <aman-goyal>
2019-06-18 21:21:30 +0530

    Vasya the Hipster Better | Alernate Solution Completed

--- Vasya the Hipster.py
+++ Vasya the Hipster.py
@@ -1,3 +1,6 @@
 red, blue = map(int, input().split())
-big, small = max(red, blue), min(red, blue)
-print(small, (big-small)//2)
+print(min(red, blue), abs(red-blue)//2)
+
+# red, blue = map(int, input().split())
+# big, small = max(red, blue), min(red, blue)
+# print(small, (big-small)//2)
