c8e10b12a663db2e37658cf100283eb6ffd2184f
Aman2 <aman-goyal>
2019-06-23 00:27:15 +0530

    The New Year: Meeting Friend Better Solution Completed

--- The New Year Meeting Friends.py
+++ The New Year Meeting Friends.py
@@ -1,2 +1,5 @@
-a,b,c = map(int, input().split())
-print( abs(a-b) + abs(b-c) + abs(a-c) - max(abs(a-b),abs(b-c),abs(a-c)))
+l = list(map(int, input().split()))
+print(max(l) - min(l))
+
+# a,b,c = map(int, input().split())
+# print( abs(a-b) + abs(b-c) + abs(a-c) - max(abs(a-b),abs(b-c),abs(a-c)))
