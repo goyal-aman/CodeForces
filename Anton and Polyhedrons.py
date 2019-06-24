2cf4b03aa99d35725e5364420b5694da1097ab2b
Aman2 <aman-goyal>
2019-06-25 01:18:54 +0530

    Anton and Polyhedrons Alternate Solution Completed

--- Anton and Polyhedrons.py
+++ Anton and Polyhedrons.py
@@ -1,11 +1,29 @@
-l = {'Tetrahedron': 4,
+from collections import Counter
+source = {'Tetrahedron': 4,
      'Cube': 6,
      'Octahedron': 8,
      'Dodecahedron': 12,
      'Icosahedron':20
-     }
+          }
 face = 0
+s = []
 for _ in range(int(input())):
-    s = input()
-    face += l[s]
+    s.append(input())
+s = dict(Counter(s))
+for i in s:
+    face+= s[i] * source[i]
 print(face)
+# _________________________
+
+
+# l = {'Tetrahedron': 4,
+#      'Cube': 6,
+#      'Octahedron': 8,
+#      'Dodecahedron': 12,
+#      'Icosahedron':20
+#      }
+# face = 0
+# for _ in range(int(input())):
+#     s = input()
+#     face += l[s]
+# print(face)
