e211b31516562cbb76054eece2080b317fcdd6d3
Aman2 <aman-goyal>
2019-06-25 01:38:36 +0530

    Choosing Teams Alternate Solution | Better Solution Completed

--- Choosing Teams.py
+++ Choosing Teams.py
@@ -1,5 +1,9 @@
 n,k = map(int, input().split())
 s = list(map(int, input().split()))
-i: int
-t = [i for i in s if i <=5-k]
-print(int(len(t)/3))
+print(sum(i<=5-k for i in s)//3)
+
+# n,k = map(int, input().split())
+# s = list(map(int, input().split()))
+# i: int
+# t = [i for i in s if i <=5-k]
+# print(int(len(t)/3))
