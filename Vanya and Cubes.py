4610b485ac555f73ccb3c9d1c854eed54fc219e3
Aman2 <aman-goyal>
2019-06-26 02:29:28 +0530

    Vanya and Cubes Alternate Solution Completed

--- Vanya and Cubes.py
+++ Vanya and Cubes.py
@@ -1,7 +1,15 @@
 n = int(input())
-temp = 2
-while n>=sum(range(1,temp)):
+i = 1
+total_cube = 0
+while total_cube <= n:
+    total_cube += (i*(i+1))//2
+    i+=1
+print(i-2)
 
-    n -= sum(range(1,temp))
-    temp +=1
-print(temp-2)
+# n = int(input())
+# temp = 2
+# while n>=sum(range(1,temp)):
+#
+#     n -= sum(range(1,temp))
+#     temp +=1
+# print(temp-2)
