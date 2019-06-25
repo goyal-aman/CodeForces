37e57a9a6e93cfaa41a9f9462441bb6136c2f5ca
Aman2 <aman-goyal>
2019-06-26 01:56:53 +0530

    Police and Recruits Alternate Solution Completed

--- Police Recruits.py
+++ Police Recruits.py
@@ -1,10 +1,19 @@
 input()
-s = list(map(int, input().split()))
-recruit = 0
-untreated = 0
-for i in s:
-    if i < 0 and recruit==0:
-        untreated+=1
-    else:
-        recruit +=i
+recruit, untreated = 0,0
+for i in map(int, input().split()):
+    recruit +=i
+    untreated += recruit<0
+    recruit = max(0, recruit)
 print(untreated)
+
+
+# input()
+# s = list(map(int, input().split()))
+# recruit = 0
+# untreated = 0
+# for i in s:
+#     if i < 0 and recruit==0:
+#         untreated+=1
+#     else:
+#         recruit +=i
+# print(untreated)
