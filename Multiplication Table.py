f2d71833752ebfb3e27d09ba9fe78fdc0c8b94eb
Aman2 <aman-goyal>
2019-07-05 01:29:42 +0530

    Multiplication Table Alternate Solution | Better Solution Completed

--- Multiplication Table.py
+++ Multiplication Table.py
@@ -3,6 +3,17 @@ k = 0
 k = 0
 for i in range(1,n+1):
     z = x/i
-    if z==int(z) and z<=n:
+    if x%i==0 and x//i<=n:
         k+=1
 print(k)
+
+
+
+# n, x = map(int, input().split())
+# k = 0
+# k = 0
+# for i in range(1,n+1):
+#     z = x/i
+#     if z==int(z) and z<=n:
+#         k+=1
+# print(k)
