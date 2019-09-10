517f6d64b89b86c15d183ba8fcb187b570745529
goyal-aman <goyal.amanrocks@gmail.com>
2019-09-10 21:23:29 +0530

    Modified Backgold Problem

--- Bachgold Problem.py
+++ Bachgold Problem.py
@@ -1,6 +1,11 @@
-n = int(input())
+n =int(input())
 print(n//2)
-if n%2==0:
-    print(*[2]*(n//2))
-else:
-    print(*[2]*(n//2 - 1), 3)    
+print('2 ' *(n//2 - 1),[2,3][n%2])
+
+
+# n = int(input())
+# print(n//2)
+# if n%2==0:
+#     print(*[2]*(n//2))
+# else:
+#     print(*[2]*(n//2 - 1), 3)    
