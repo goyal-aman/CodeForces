b899fc3ce532a0867a233a296f9eea869dcc464a
goyal-aman <goyal.amanrocks@gmail.com>
2019-11-10 15:26:50 +0530

    solution2: hit the lottery

--- Hit the Lottery.py
+++ Hit the Lottery.py
@@ -3,10 +3,17 @@ by goyal-aman
 https://codeforces.com/problemset/problem/996/A
 """
 
-n = int(input())
-currency = [1 , 5, 10, 20, 100]
-notes = 0
-for val in currency[::-1]:
+def notes(money):
+  currency = [1 , 5, 10, 20, 100]
+  notes = 0
+  for val in currency[::-1]:
     notes += n//val
     n = n%val
-print(notes)
+  print(notes)
+
+
+#solution 2
+n = int(input)
+def notes2(money):
+  print( (money//100) + (money%100)//20 + (money%20)//10 + (money%10)//5 + money%5)
+
